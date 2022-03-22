1bfbc64564f155a12e2dc3d6341833463b033ff2
VehicleLoanProject <101493941+VehicleLoanProject@users.noreply.github.com>
2022-03-22 09:36:11 +0530

    Add files via upload

--- VehicleLoan/VehicleLoan.BusinessModels/LoanDetailsModel.cs
+++ VehicleLoan/VehicleLoan.BusinessModels/LoanDetailsModel.cs
@@ -1,17 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
 
-namespace VehicleLoan.BusinessModels
+namespace VehicleLoan.DataAccessLayer.Models
 {
-    public class LoanDetailsModel
+    public partial class LoanDetailsModel
     {
         public int LoanAppId { get; set; }
         public decimal LoanAmount { get; set; }
         public int LoanTenure { get; set; }
         public int InterestRate { get; set; }
-        public int? CustomerId { get; set; }
-        public int? StatusId { get; set; }
+        public int CustomerId { get; set; }
+        public int StatusId { get; set; }
 
         public virtual ApplicantDetailsModel Customer { get; set; }
         public virtual ApplicationStatusModel Status { get; set; }
