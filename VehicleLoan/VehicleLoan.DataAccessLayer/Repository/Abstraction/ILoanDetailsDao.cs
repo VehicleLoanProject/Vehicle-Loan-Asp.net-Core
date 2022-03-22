252f23254c18bf7d3b9d6ee8e19e58a460ea19d3
VehicleLoanProject <101493941+VehicleLoanProject@users.noreply.github.com>
2022-03-22 09:39:14 +0530

    LoanDetails Dao created

---
+++ VehicleLoan/VehicleLoan.DataAccessLayer/Repository/Abstraction/ILoanDetailsDao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VehicleLoan.DataAccessLayer.Models;
+
+namespace VehicleLoanDataAccessLayer.Repository.Abstraction
+{
+    public interface ILoanDetailsDao
+    {
+        int AddLoanDetails(LoanDetails loanDetails);
+
+        List<LoanDetailsModel> GetAllLoanDetails(); 
+        
+        public int DeleteLoanRecord(int id);
+    }
+}
+ 
